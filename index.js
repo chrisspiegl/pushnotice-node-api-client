24b47bcd8cc8e7ab6c1d08aad95723ff03a9a68e
Chris Spiegl <chris@spiegl.io>
2017-11-03 14:03:19 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const request = require('request-promise-native');
+
+function sendRequest(namespace, text, level='INFO', options={}) {
+  if (options.chat == undefined) {
+    console.error('PushNotice: chat object has to be provided');
+    return;
+  }
+  if (options.chat && options.chat.id == undefined) {
+    console.error('PushNotice: chat id has to be provided');
+    return;
+  }
+  if (options.chat && options.chat.secret == undefined) {
+    console.error('PushNotice: chat secret has to be provided');
+    return;
+  }
+  if (namespace == undefined) {
+    console.error('PushNotice: namespace has to be provided');
+    return;
+  }
+  if (text == undefined) {
+    console.error('PushNotice: text has to be provided');
+    return;
+  }
+  if (level == undefined) {
+    console.error('PushNotice: level has to be provided');
+    return;
+  }
+  if (options.url == undefined) options.url = 'https://pushnotice.chat/api/vi/chatnotice';
+  if (options.debug == undefined) options.debug = false;
+  let requestOptions = {
+    method: 'POST',
+    uri: options.url,
+    body: {
+      chatId: options.chat.id,
+      chatSecret: options.chat.secret,
+      namespace: namespace,
+      level: level,
+      text: text,
+    },
+    json: true,
+    headers: {
+      'content-type': 'application/json',
+    }
+  };
+  request(requestOptions).then((res) => {
+    if (options.debug) console.log("Successfully sent Notification to PushNotice API");
+  }).catch((err) => {
+    if (options.debug) console.error("Error sending Notification to PushNotice API");
+  });
+}
+
+module.exports = (namespace, options={}) => {
+  return (text, level='INFO') => {
+    sendRequest(namespace, text, level, options);
+  }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# PushNotice API Client for Node.js
+
+[@PushNoticeBot](https://t.me/PushNoticeBot) is a bot that will help you send notifications directly to a chat (or group). Easy to setup and integrate.
+
+* Bot: [@PushNoticeBot](https://t.me/PushNoticeBot)
+* Website: [PushNotice.chat](https://pushnotice.chat)
+* Developer: Chris ([Telegram](https://t.me/spieglio), [Twitter](https://twitter.com/spieglio), [Email](mailto:hello@pushnotice.chat))
+
+This is work in progress and will be developed further.
