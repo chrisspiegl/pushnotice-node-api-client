4bc77a8d2dc36173fa1a7a86b8c859bfa8a07450
Chris Spiegl <chris@spiegl.io>
2017-11-03 14:09:16 +0700

    Add `Pushnotice:` to namespace log messages in debug mode.

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ function sendRequest(namespace, text, level='INFO', options={}) {
     }
   };
   request(requestOptions).then((res) => {
-    if (options.debug) console.log("Successfully sent Notification to PushNotice API");
+    if (options.debug) console.log("PushNotice: Successfully sent Notification to PushNotice API");
   }).catch((err) => {
-    if (options.debug) console.error("Error sending Notification to PushNotice API");
+    if (options.debug) console.error("PushNotice: Error sending Notification to PushNotice API");
   });
 }
 
