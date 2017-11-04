d759a1dc0bb91341fa8ee205e6a6d0e257ea1b10
Chris Spiegl <chris@spiegl.io>
2017-11-05 01:31:30 +0700

    GravatarAdd ability to disable the sending of the push notifications via a `options.disabled`. Disabled all logging if `options.debug` is not set to true. Commit: cb280fecc3df8bf9d817ae0d441b0173bd4988bd [cb280fe] Parents: a8ff9e3dbd Author: Chris Spiegl <chris@spiegl.io> Date: November 5, 2017 at 01:31:19 GMT+7 Labels: HEAD -> master

--- index.js
+++ index.js
@@ -3,28 +3,32 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const request = require('request-promise-native');
 
 function sendRequest(namespace, text, level='INFO', options={}) {
+  if (options.disabled == true) {
+    if (options.debug) console.log('PushNotice: disabled not sending the notification');
+    return;
+  }
   if (options.chat == undefined) {
-    console.error('PushNotice: chat object has to be provided');
+    if (options.debug) console.error('PushNotice: chat object has to be provided');
     return;
   }
   if (options.chat && options.chat.id == undefined) {
-    console.error('PushNotice: chat id has to be provided');
+    if (options.debug) console.error('PushNotice: chat id has to be provided');
     return;
   }
   if (options.chat && options.chat.secret == undefined) {
-    console.error('PushNotice: chat secret has to be provided');
+    if (options.debug) console.error('PushNotice: chat secret has to be provided');
     return;
   }
   if (namespace == undefined) {
-    console.error('PushNotice: namespace has to be provided');
+    if (options.debug) console.error('PushNotice: namespace has to be provided');
     return;
   }
   if (text == undefined) {
-    console.error('PushNotice: text has to be provided');
+    if (options.debug) console.error('PushNotice: text has to be provided');
     return;
   }
   if (level == undefined) {
-    console.error('PushNotice: level has to be provided');
+    if (options.debug) console.error('PushNotice: level has to be provided');
     return;
   }
   if (options.url == undefined) options.url = 'https://pushnotice.chat/api/v1/chatnotice';
