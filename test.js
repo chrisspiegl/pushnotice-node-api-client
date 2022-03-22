54713abcf4337d4b98bcade595eafab420ed4add
Chris Spiegl <chris@chrisspiegl.com>
2022-03-22 11:53:00 +0100

    Disable tests when no config.

--- test.js
+++ test.js
@@ -9,87 +9,89 @@ const config = {
   },
 }
 
-console.log('Test file currently being run:', test.meta.file)
+if (config.chat.id !== '') {
+  console.log('Test file currently being run:', test.meta.file)
 
-test('handle options.disabled = true', async (t) => {
-  const pn = pnotice('pntest', {
-    disabled: true,
-    debug: true,
-    chat: config.chat,
-    env: 'test',
+  test('handle options.disabled = true', async (t) => {
+    const pn = pnotice('pntest', {
+      disabled: true,
+      debug: true,
+      chat: config.chat,
+      env: 'test',
+    })
+    const response = await pn('hello world', 'INFO')
+    t.is(response?.error?.title, 'Disabled')
   })
-  const response = await pn('hello world', 'INFO')
-  t.is(response?.error?.title, 'Disabled')
-})
 
-test('handle options.chat.id = undefined', async (t) => {
-  const pn = pnotice('pntest', {
-    disabled: false,
-    debug: true,
-    chat: {
-      id: undefined,
-      secret: config.chat.secret,
-    },
-    env: 'test',
+  test('handle options.chat.id = undefined', async (t) => {
+    const pn = pnotice('pntest', {
+      disabled: false,
+      debug: true,
+      chat: {
+        id: undefined,
+        secret: config.chat.secret,
+      },
+      env: 'test',
+    })
+    const response = await pn('hello world', 'INFO')
+    t.is(response?.error?.title, 'MissingChatId')
   })
-  const response = await pn('hello world', 'INFO')
-  t.is(response?.error?.title, 'MissingChatId')
-})
 
-test('handle options.chat.secret = undefined', async (t) => {
-  const pn = pnotice('pntest', {
-    disabled: false,
-    debug: true,
-    chat: {
-      id: config.chat.id,
-      secret: undefined,
-    },
-    env: 'test',
+  test('handle options.chat.secret = undefined', async (t) => {
+    const pn = pnotice('pntest', {
+      disabled: false,
+      debug: true,
+      chat: {
+        id: config.chat.id,
+        secret: undefined,
+      },
+      env: 'test',
+    })
+    const response = await pn('hello world', 'INFO')
+    t.is(response?.error?.title, 'MissingChatSecret')
   })
-  const response = await pn('hello world', 'INFO')
-  t.is(response?.error?.title, 'MissingChatSecret')
-})
 
-test('handle options.chat = undefined', async (t) => {
-  const pn = pnotice('pntest', {
-    disabled: false,
-    debug: true,
-    chat: undefined,
-    env: 'test',
+  test('handle options.chat = undefined', async (t) => {
+    const pn = pnotice('pntest', {
+      disabled: false,
+      debug: true,
+      chat: undefined,
+      env: 'test',
+    })
+    const response = await pn('hello world', 'INFO')
+    t.is(response?.error?.title, 'MissingChatObject')
   })
-  const response = await pn('hello world', 'INFO')
-  t.is(response?.error?.title, 'MissingChatObject')
-})
 
-test('handle text being an object', async (t) => {
-  const pn = pnotice('pntest', {
-    disabled: false,
-    debug: true,
-    chat: config.chat,
-    env: 'test',
+  test('handle text being an object', async (t) => {
+    const pn = pnotice('pntest', {
+      disabled: false,
+      debug: true,
+      chat: config.chat,
+      env: 'test',
+    })
+    const response = await pn({ i: 'am an object' }, 'INFO')
+    t.is(response?.error?.title, 'TextMustBeString')
   })
-  const response = await pn({ i: 'am an object' }, 'INFO')
-  t.is(response?.error?.title, 'TextMustBeString')
-})
 
-test('handle namespace being an object', async (t) => {
-  const pn = pnotice({ i: 'am an object' }, {
-    disabled: false,
-    debug: true,
-    chat: config.chat,
-    env: 'test',
+  test('handle namespace being an object', async (t) => {
+    const pn = pnotice({ i: 'am an object' }, {
+      disabled: false,
+      debug: true,
+      chat: config.chat,
+      env: 'test',
+    })
+    const response = await pn('hello world', 'INFO')
+    t.is(response?.error?.title, 'NamespaceMustBeString')
   })
-  const response = await pn('hello world', 'INFO')
-  t.is(response?.error?.title, 'NamespaceMustBeString')
-})
 
-test('handle successfully sent', async (t) => {
-  const pn = pnotice('pntest', {
-    disabled: false,
-    debug: true,
-    chat: config.chat,
-    env: 'test',
+  test('handle successfully sent', async (t) => {
+    const pn = pnotice('pntest', {
+      disabled: false,
+      debug: true,
+      chat: config.chat,
+      env: 'test',
+    })
+    const response = await pn('hello world', 'INFO')
+    t.is(response?.status, 'ok')
   })
-  const response = await pn('hello world', 'INFO')
-  t.is(response?.status, 'ok')
-})
+}
