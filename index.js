92657e2b0fa5436a4ca0b87e1e5d9cf0dc7e06a3
Chris Spiegl <chris@chrisspiegl.com>
2022-03-22 11:31:26 +0100

    update to module and use new xo formatting.

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
-const fetch = require('isomorphic-unfetch')
+import fetch from 'isomorphic-unfetch'
 
 function checkInputs(namespace, text, level, options) {
-  if (options.url === undefined) options.url = 'https://pushnotice.chat/api/v1/chatnotice'
-  if (options.debug === undefined) options.debug = false
-  if (options.env === undefined) options.env = undefined
+  if (options.url === undefined) {
+    options.url = 'https://pushnotice.chat/api/v1/chatnotice'
+  }
+
+  if (options.debug === undefined) {
+    options.debug = false
+  }
+
+  if (options.env === undefined) {
+    options.env = undefined
+  }
+
   if (options.disabled === true) {
     return { error: { title: 'Disabled', message: 'disabled not sending the notification' } }
   }
+
   if (options.chat === undefined) {
     return { error: { title: 'MissingChatObject', message: 'chat object has to be provided' } }
   }
+
   if (options.chat?.id === undefined) {
     return { error: { title: 'MissingChatId', message: 'chat id has to be provided' } }
   }
+
   if (options.chat?.secret === undefined) {
     return { error: { title: 'MissingChatSecret', message: 'chat secret has to be provided' } }
   }
+
   if (namespace === undefined) {
     return { error: { title: 'MissingNamespace', message: 'namespace has to be provided' } }
   }
+
   if (typeof namespace !== 'string' && !(namespace instanceof String)) {
     return { error: { title: 'NamespaceMustBeString', message: 'namespace must be a String' } }
   }
+
   if (text === undefined && text === '') {
     return { error: { title: 'MissingText', message: 'text has to be provided' } }
   }
+
   if (typeof text !== 'string' && !(text instanceof String)) {
     return { error: { title: 'TextMustBeString', message: 'text must be a String' } }
   }
+
   if (level === undefined) {
     return { error: { title: 'MissingLevel', message: 'level has to be provided' } }
   }
+
   if (typeof level !== 'string' && !(level instanceof String)) {
     return { error: { title: 'LevelMustBeString', message: 'level must be a String' } }
   }
@@ -65,7 +83,7 @@ async function sendRequest(namespace, text, level = 'INFO', options = {}) {
     if (!body.ok && body.name !== 'notificationQueued') {
       return { error: { title: 'ApiRequestFailed', message: 'Error sending Notification to PushNotice API' } }
     }
-  } catch (err) {
+  } catch {
     return { error: { title: 'ApiRequestFailed', message: 'Error sending Notification to PushNotice API' } }
   }
 
@@ -76,4 +94,4 @@ function pnotice(namespace, options = {}) {
   return (text, level = 'INFO') => sendRequest(namespace, text, level, options)
 }
 
-module.exports = pnotice
+export default pnotice
